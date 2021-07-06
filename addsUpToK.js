956751f7f8958caac14b7d7591a21cc7eca742c2
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-07-06 18:30:33 +0200

    add new code

---
+++ addsUpToK.js
@@ -0,0 +1,31 @@
+/*
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+
+input:
+an array of numbers
+
+
+output:
+
+*/
+
+const addsUpToK = (arr, k) => {
+  const hash = {};
+  for (let i = 0; i < arr.length; i += 1) {
+    const value = arr[i];
+    if (hash[value]) {
+      return true;
+    }
+    hash[k - value] = true;
+  };
+  return false;
+}
+
+const arr = [10, 15, 3, 7];
+const k = 17;
+
+console.log(addsUpToK(arr, k)); // this should log true
---
+++ fetch.js
@@ -0,0 +1,4 @@
+fetch("https://api.thecatapi.com/v1/images/search")
+  .then(data => {
+    console.log(data);
+  })
